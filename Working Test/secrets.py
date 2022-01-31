624cb7f83ec6fe68fffa14066fbc310d4fc4e62f
iainmacthai <iainmacthai@users.noreply.github.com>
2022-01-31 07:27:18 +0000

    Add files via upload

---
+++ Working Test/secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'TENTEL',
+    'password' : 'flipflop5187',
+    'aio_username' : 'iainmac',
+    'aio_key' : '36eee05e6f4223127e403bb7f77fefd8f17f2ee0',
+    'timezone' : "Europe/London", # http://worldtimeapi.org/timezones
+    'broker' : 'io.adafruit.com',
+    'openweather_token' : '687b8d90a43e64fb1fde9ae9b5b20721',
+    'openweather_location' : 'Thief River Falls, US',
+    'port' : 1883
+    }
